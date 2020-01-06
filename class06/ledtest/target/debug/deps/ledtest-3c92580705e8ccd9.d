3093e638f35aa99c22ed6f110cb127df42d93d9e
Hunain Arbani <hunain.arbani@yahoo.com>
2020-01-06 20:58:01 +0500

    class07

---
+++ class06/ledtest/target/debug/deps/ledtest-3c92580705e8ccd9.d
@@ -0,0 +1,5 @@
+/home/hunain/Desktop/IOTQ2/class06/ledtest/target/debug/deps/ledtest-3c92580705e8ccd9: src/main.rs
+
+/home/hunain/Desktop/IOTQ2/class06/ledtest/target/debug/deps/ledtest-3c92580705e8ccd9.d: src/main.rs
+
+src/main.rs:
---
+++ class06/ledtest/target/debug/ledtest.d
@@ -0,0 +1,1 @@
+/home/hunain/Desktop/IOTQ2/class06/ledtest/target/debug/ledtest: /home/hunain/Desktop/IOTQ2/class06/ledtest/src/main.rs
