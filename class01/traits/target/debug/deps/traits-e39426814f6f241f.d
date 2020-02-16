f3a59310fdd88462b5fb27e7932926afa20471fd
Hunain Arbani <hunain.arbani@yahoo.com>
2020-02-16 12:07:42 +0500

    class09feb2020

---
+++ class01/traits/target/debug/deps/traits-e39426814f6f241f.d
@@ -0,0 +1,5 @@
+/home/hunain/Desktop/iotq2/class01/traits/target/debug/deps/traits-e39426814f6f241f: src/main.rs
+
+/home/hunain/Desktop/iotq2/class01/traits/target/debug/deps/traits-e39426814f6f241f.d: src/main.rs
+
+src/main.rs:
--- class01/traits/target/debug/traits.d
+++ class01/traits/target/debug/traits.d
@@ -1,1 +1,1 @@
-/home/hunain/Desktop/IOTQ2/class01/traits/target/debug/traits: /home/hunain/Desktop/IOTQ2/class01/traits/src/main.rs
+/home/hunain/Desktop/iotq2/class01/traits/target/debug/traits: /home/hunain/Desktop/iotq2/class01/traits/src/main.rs
